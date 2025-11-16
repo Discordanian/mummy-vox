6dc83ba5500864e012f3528a2f29d574794770a9
Kurt Schwind <kurt.schwind@gmail.com>
2025-11-15 19:22:27 -0600

    Settings Menu Complete

--- README.md
+++ README.md
@@ -2,3 +2,6 @@
 Random audio track playing
 
 Hoping to make this a web release.
+
+# Attributions
+- Switch ON - waterheater.wav by MastersDisaster -- https://freesound.org/s/218120/ -- License: Attribution 4.0
--- godot/scripts/settings.gd
+++ godot/scripts/settings.gd
@@ -1,21 +1,37 @@
 extends Control
 
-@export var main_volume: HSlider
+@export var main_volume: HSlider   
+@export var voice_volume: HSlider
 @export var sfx_volume: HSlider
 
+@onready var SFX_BUS_ID = AudioServer.get_bus_index("SFX")
+@onready var VOICE_BUS_ID = AudioServer.get_bus_index("Voice")
+@onready var MAIN_BUS_ID = AudioServer.get_bus_index("Master")
 
-# Called when the node enters the scene tree for the first time.
+
+
 func _ready() -> void:
-    # If local values present use them.
-    main_volume.value = 80
-    sfx_volume.value = 40
+    main_volume.value = AudioServer.get_bus_volume_linear(MAIN_BUS_ID)
+    sfx_volume.value = AudioServer.get_bus_volume_linear(SFX_BUS_ID)
+    voice_volume.value = AudioServer.get_bus_volume_linear(VOICE_BUS_ID)
 
 
-func _on_main_volume_drag_ended(value_changed: bool) -> void:
-    if value_changed:
-        print("New value of Master Volume ", main_volume.value)
+func _on_sfx_volume_value_changed(value: float) -> void:
+    print("New value of SFX Volume ", sfx_volume.value) 
+    AudioServer.set_bus_volume_db(SFX_BUS_ID, linear_to_db(value))
+    AudioServer.set_bus_mute(SFX_BUS_ID, value < 0.05)
 
 
-func _on_sfx_volume_drag_ended(value_changed: bool) -> void:
-    if value_changed:
-        print("New value of SFX Volume ", sfx_volume.value) # Replace with function body.
+func _on_main_volume_value_changed(value: float) -> void:
+    print("New value of MAIN Volume ", sfx_volume.value) 
+    AudioServer.set_bus_volume_db(MAIN_BUS_ID, linear_to_db(value))
+    AudioServer.set_bus_mute(MAIN_BUS_ID, value < 0.05)
+
+func _on_voice_volume_value_changed(value: float) -> void:
+    print("New value of VOX Volume ", sfx_volume.value) 
+    AudioServer.set_bus_volume_db(VOICE_BUS_ID, linear_to_db(value))
+    AudioServer.set_bus_mute(VOICE_BUS_ID, value < 0.05)
+
+
+func _on_return_button_button_down() -> void:
+    print("Return to Main")
